1c87966b712b1a55db74683f90b16ee5291f8ad7
Primus <prisi581@student.liu.se>
2025-06-17 16:47:07 +0200

    started on QR code generator

---
+++ QR-code-generator-in-C/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "qr.h"
+
+
+int main()
+{
+  printf("%s\n", "test");
+}
---
+++ QR-code-generator-in-C/qr.h
@@ -0,0 +1,28 @@
+#ifndef QR_H
+#define QR_H
+
+
+
+#include <stdint.h>
+
+
+
+typedef struct QR
+{
+  uint8_t version;
+} QR_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
