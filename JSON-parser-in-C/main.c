baad1f58bbf8a9df0ed5d8b891c5927efeb47456
Primus <prisi581@student.liu.se>
2025-06-19 02:33:32 +0200

    initial stuff

---
+++ JSON-parser-in-C/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ps_JSON.h"
+
+
+
+int main(void)
+{
+  printf("hello world\n"); 
+}
---
+++ JSON-parser-in-C/ps_JSON.h
@@ -0,0 +1,24 @@
+#ifndef PS_JSON_H
+#define PS_JSON_H
+
+
+
+typedef struct element
+{
+    int t;
+} element_t;
+
+
+typedef struct JSON
+{
+    element_t* element_ptr;
+} JSON_t;
+
+
+
+
+
+
+
+
+#endif
